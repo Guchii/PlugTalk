4ed24788f1ba23373fe885221916adde0b7069f5
Guchii <71770229+Guchii@users.noreply.github.com>
2021-10-09 10:59:27 +0530

    Nice Login Page

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -19,32 +19,36 @@ const LoginPage = () => {
                 </div>
             </nav>
             <div
-                className="d-flex align-items-center justify-content-center"
+                className="d-flex align-items-center justify-content-center bg-dark"
                 style={{
-                    backgroundColor: " #04000A",
                     height: "calc(100vh - 80px - 56px)",
                     margin: 0,
                     padding: 0,
                 }}
             >
-                <div className="text-center">
-                    <p className="fs-4 text-light">
-                        Hello {name ? name : "Friend"}
+                <div className="card p-5 text-center shadow-lg">
+                    <p className="fs-4 text-dark">
+                        Hello {name ? name : ""}
                     </p>
                     <form action="" className="">
                         <div className="mb-3">
                             <input
-                                className="form-control bg-dark text-light"
+                                className="form-control"
                                 type="text"
                                 placeholder="UserName"
                                 onChange={(e) => {
-                                    setName(e.target.value);
+                                    let nice = e.target.value;
+                                    if(nice.length>18){
+                                        nice = nice.substring(0,18);
+                                    }
+                                    e.target.value = nice;
+                                    setName(nice);
                                 }}
                             />
                         </div>
                         <div className="mb-3">
                             <input
-                                className="form-control bg-dark text-light"
+                                className="form-control"
                                 type="password"
                                 placeholder="Password"
                             />
@@ -52,31 +56,31 @@ const LoginPage = () => {
                         <div className="btn-group mt-2">
                             <button
                                 onClick=""
-                                className="btn btn-dark"
+                                className="btn btn-outline-primary"
                                 disabled
                             >
                                 Login
                             </button>
                             <button
                                 onClick=""
-                                className="btn btn-dark"
+                                className="btn btn-outline-success"
                                 disabled
                             >
                                 Signup
                             </button>
                         </div>
                     </form>
-                    <button className="btn bg-warning mt-4" onClick={signIn}>
+                    <button className="btn bg-warning mt-4 position-relative " onClick={signIn}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="16"
                             height="16"
                             fill="currentColor"
-                            class="bi bi-google"
+                            className="bi bi-google"
                             viewBox="0 0 16 16"
                         >
                             <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-                        </svg>
+                        </svg>{" "}
                         Sign in with Google
                     </button>
                 </div>
